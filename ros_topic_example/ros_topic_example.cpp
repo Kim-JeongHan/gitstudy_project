63f021ac9f11aa2d3b4ffa39e975f6030c15f054
taehun-ryu <xogns2079@gmail.com>
2023-07-18 15:49:57 +0900

    feat: Create ros_topic_example.cpp file.

---
+++ ros_topic_example/ros_topic_example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class ClassName
+{
+ public:
+  ClassName(/* args */);
+  ~ClassName();
+
+ private:
+  /* data */
+};
+
+ClassName::ClassName(/* args */)
+{
+}
+
+ClassName::~ClassName()
+{
+}
+
+int main()
+{
+  
+
+  return 0;
+}
